89e07dd8c490726f5f1a3f1cdff1859cdaa6ea03
root <root@ltsp>
2017-03-29 19:50:35 -0300

    Movimiento pelota

--- pong.py
+++ pong.py
@@ -9,25 +9,27 @@ alto = 480
 pantalla = pygame.display.set_mode((ancho,alto))
 
 fondo = pygame.image.load("imagenes/fondo1.jpg")
-pelota = pygame.image.load("imagenes/pelota1.png")
 paleta_jugador = pygame.image.load("imagenes/paleta.png")
 paleta_ia = pygame.image.load("imagenes/paleta.png")
 
 # bolax = 100
 # bolay = 300
-bolavelx = 20
 
 #Definir Sprite Pelota
-
+pelota = pygame.image.load("imagenes/pelota1.png")
 sprite_pelota = pygame.sprite.Sprite()
 sprite_pelota.image = pelota 
-
-#Toma el ancho y alto de la imagen
 sprite_pelota.rect = pelota.get_rect()
-
-# Velocidad Sprite
 sprite_pelota.rect.x = 100
 sprite_pelota.rect.y = 100
+
+#Velocidad
+bolavelx = 10
+bolavely = 10
+
+#Reloj
+clock = pygame.time.Clock()
+FPS = 60
 
 while True:
 	
@@ -40,15 +42,23 @@ while True:
 			sys.exit()
 	
 	#Movimiento bolax
-	# bolax += bolavelx
+	sprite_pelota.rect.x += bolavelx
 	
 	#fondo
 	pantalla.blit(fondo,(0,0))
 	
 	#Posicion Pelota
-	# pantalla.blit(sprite_pelota.image, sprite_pelota.rect)
-	sprite_pelota.rect.x = bolavelx
+	pantalla.blit(sprite_pelota.image, sprite_pelota.rect)
 	
+	if sprite_pelota.rect.x+ sprite_pelota.rect.width > ancho:
+		bolavelx = bolavelx *-1
+		
+	if sprite_pelota.rect.x < 0:
+		bolavelx = bolavelx *-1
+		
+		
+	# sprite_pelota.rect.x = bolavelx
+	print clock.tick(FPS)
 	
 	#otros objetos
 	pantalla.blit(paleta_jugador, (50,150))
